df3fcae9ac17be7d86f7ca9978523591dd444df9
orell trautmann <orell.trautmann@uni-rostock.de>
2026-05-05 10:18:20 +0200

    added a python script to change the node indexation so the testing procedure works properly

---
+++ reformat_datasets.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Multiplex network embedding tester pipeline')
+    parser.add_argument('-p', '--path', type=str, default='Datasets/Vickers/Vickers-Chan-7thGraders_multiplex.edges.txt',
+    					help='path to the dataset')
+    return parser.parse_args()
+
+def read_csv(file_path: str) -> pd.DataFrame:
+    df = pd.read_csv(file_path, sep=" ", header=None)
+    df.rename(columns={0:"layer", 1:"source", 2:"target", 3:"weight"}, inplace=True)
+    return df
+
+def reindex_nodes(file_path: str) -> None:
+    df = read_csv(file_path)
+    node_list = list(set(df["source"]).union(set(df["target"])))
+    df.loc[:,["source", "target"]] = df.loc[:,["source", "target"]].map(lambda x: node_list.index(x))
+    df.to_csv(file_path, sep=" ", index=False, header=False)
+    
+
+if __name__ == "__main__":
+    args = parse_args()
+    reindex_nodes(args.path)
+    
+
